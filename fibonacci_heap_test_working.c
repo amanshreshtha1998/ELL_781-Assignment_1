b09947024035bb4daf9ac8449d4dc3e26328cf19
Aman Shreshtha <32384584+amanshreshtha1998@users.noreply.github.com>
2020-11-21 12:47:21 +0530

    Add files via upload

--- fibonacci_heap_test_working.c
+++ fibonacci_heap_test_working.c
@@ -1,241 +1,107 @@
-#include"fibonacci_heap_utils.h"
+#include"fibonacci_heap_utils_working.h"
 
 int main()
 {
-	/*HEAP * my_heap;
-
-	my_heap = make_fib_heap();
-
-	insert(my_heap,7);
-
-	insert(my_heap,18);
-
-	insert(my_heap,38);
-
-	printf("Calling find_node on node of val 7\n");
-	NODE * n = find_node(my_heap->min,7);
-	if( n == NULL )
-	{
-		printf(" Node not found \n ");
-	}else{
-
-		printf(" Node found ! \n");
-	}
-
-	NODE * d1 = create_dummy_node(24);
-	n->child = d1;
-	d1->parent = n;
-	(my_heap->n)++;
-
-	NODE * d2 = create_dummy_node(26);
-	d2->mark = 'T';
-	(n->child)->child = d2;
-	d2->parent = (n->child);
-	(my_heap->n)++;
-
-	NODE * d3 = create_dummy_node(46);
-	d3->parent = (n->child);
-	(n->child->child)->right = d3;
-	d3->left =  (n->child->child);
-	(n->child->child)->left = d3;
-	d3->right = (n->child->child);
-	(my_heap->n)++;
-
-	NODE * d4 = create_dummy_node(35);
-	n->child->child->child = d4;
-	d4->parent = n->child->child;
-	(my_heap->n)++;
-
-	/*NODE * m = find_node(my_heap->min , 49);
-	if(m == NULL)
-	{
-		printf("node not found \n");
-	}
-	m->key = 15;
-	printf("%d\n",m->key );
-	printf("%d\n",m->parent->key);*/
-
-	/*print_heap(my_heap->min);
-	printf("\n");
-
-	printf("Printing mark of node of val 24 : %c\n",n->child->mark);
-
-	printf("Calling decrease key on node 46 \n");
-	decrease_key(my_heap,46,15);
-
-	printf("Printing mark of node of val 24 : %c\n",n->child->mark);
-
-	print_heap(my_heap->min);
-	printf("\n");
-
-	printf("minimum value is : %d\n",my_heap->min->key);	
-
-	printf("Calling decrease key on node 35 \n");
-	decrease_key(my_heap,35,5);
-
-	printf("minimum value is : %d\n",my_heap->min->key);
-
-	insert(my_heap,6);
-
-	print_heap(my_heap->min);
-	printf("\n");
-
-	insert(my_heap,4);
-
-	print_heap(my_heap->min);
-	printf("\n");
-
-	printf("minimum value is : %d\n",my_heap->min->key);
-
-	return 0;*/
-
 	HEAP * my_heap;
 
 	my_heap = make_fib_heap();
 
-	insert(my_heap,7);
+	printf("Enter number of root list elements.\n");
 
-	insert(my_heap,23);
+	int rootNum;
 
-	insert(my_heap,21);
+	scanf("%d",&rootNum);
 
-	insert(my_heap,3);
-
-	insert(my_heap,17);
-
-	insert(my_heap,24);
-
-	printf("Calling find_node on node of val 3\n");
-	NODE * n = find_node(my_heap->min,3);
-	if( n == NULL )
+	for(int i=0;i<rootNum;i++)
 	{
-		printf(" Node not found \n ");
-	}else{
+		int key;
 
-		printf(" Node found ! \n");
+		scanf("%d",&key);
+
+		insert(my_heap,key);
 	}
 
-	NODE * d1 = create_dummy_node(18);
-	d1->mark = 'T';
-	n->child = d1;
-	n->degree+=1;
-	d1->parent = n;
-	(my_heap->n)++;
+	int non_rootNum;
 
-	NODE * d2 = create_dummy_node(39);
-	d2->mark = 'T';
-	(n->child)->child = d2;
-	d2->parent = (n->child);
-	d2->parent->degree+=1;
-	(my_heap->n)++;
+	printf("Enter number of non-root list elements. \n");
 
-	NODE * d3 = create_dummy_node(52);
-	d3->parent = n;
-	n->degree+=1;
-	(n->child)->right = d3;
-	d3->left =  (n->child);
-	(n->child)->left = d3;
-	d3->right = (n->child);
-	(my_heap->n)++;
+	scanf("%d",&non_rootNum);
 
-	NODE * d4 = create_dummy_node(38);
-	d4->parent = n;
-	n->degree+=1;
-	(n->child->right)->right = d4;
-	d4->left =  (n->child->right);
-	(n->child)->left = d4;
-	d4->right = (n->child);
-	(my_heap->n)++;
-
-	NODE * d5 = create_dummy_node(41);
-	d5->parent = n->child->right->right;
-	n->child->right->right->degree += 1;
-	n->child->right->right->child = d5;
-	(my_heap->n)++;
-
- 	printf("Calling find_node on node of val 17\n");
-	n = find_node(my_heap->min,17);
-	if( n == NULL )
+	for(int i=0;i<non_rootNum;i++)
 	{
-		printf(" Node not found \n ");
-	}else{
+		int main_node;
+		int CorR;
+		int key;
+		bool mark;
+		int temp;
 
-		printf(" Node found ! \n");
+		scanf("%d %d %d %d",&main_node,&CorR,&key,&temp);
+		mark = temp;
+
+		NODE * n = find_node(my_heap->min,main_node);
+
+		if( CorR == 0 )
+		{
+			NODE * d = create_dummy_node(key);
+			d->mark = mark;
+			n->child = d;
+			n->degree+=1;
+			d->parent = n;
+			(my_heap->n)++;
+		}
+		if( CorR == 1 )
+		{
+			NODE * d = create_dummy_node(key);
+			d->mark = mark;
+			d->parent = n->parent;
+			n->parent->degree+=1;
+
+			( n -> left ) -> right = d;
+			d->right = n;
+			d->left = n->left;
+			n->left = d;
+			(my_heap->n)++;		
+		}
+
 	}
 
-	NODE * d6 = create_dummy_node(30);
-	n->child = d6;
-	n->degree+=1;
-	d6->parent = n;
-	(my_heap->n)++;
 
-	printf("Calling find_node on node of val 24\n");
-	n = find_node(my_heap->min,24);
-	if( n == NULL )
-	{
-		printf(" Node not found \n ");
-	}else{
-
-		printf(" Node found ! \n");
-	}
-
-	NODE * d7 = create_dummy_node(26);
-	d7->mark = 'T';
-	n->child = d7;
-	n->degree+=1;
-	d7->parent = n;
-	(my_heap->n)++;
-
-	NODE * d8 = create_dummy_node(35);
-	(n->child)->child = d8;
-	d8->parent = (n->child);
-	d8->parent->degree+=1;
-	(my_heap->n)++;
-
-	NODE * d9 = create_dummy_node(46);
-	d9->parent = n;
-	n->degree+=1;
-	(n->child)->right = d9;
-	d9->left =  (n->child);
-	(n->child)->left = d9;
-	d9->right = (n->child);
-	(my_heap->n)++;
-
-	/*NODE * m = find_node(my_heap->min , 49);
-	if(m == NULL)
-	{
-		printf("node not found \n");
-	}
-	m->key = 15;
-	printf("%d\n",m->key );
-	printf("%d\n",m->parent->key);*/
-
+	printf("\n\n*************************************************** \n\n");
 	print_heap(my_heap->min);
+	printf("\n\n*************************************************** \n\n");
 
 	printf("number of nodes in my_heap is %d \n", my_heap->n);
 	printf("\n");
 	
-	printf ("calling extract_min on H \n");
+	printf ("calling extract_min on H \n\n");
 	ext_min(my_heap);
 
-	printf("extract min done \n");
+	printf("\nextract min done \n");
 
+	printf("\n\n*************************************************** \n\n");
 	print_heap(my_heap->min);
+	printf("\n\n*************************************************** \n\n");
 
 	printf("Calling decrease key on node 46 \n");
 	decrease_key(my_heap,46,15);
 
 	//printf("Printing mark of node of val 24 : %c\n",n->child->mark);
 
+	printf("\n\n*************************************************** \n\n");
 	print_heap(my_heap->min);
+	printf("\n\n*************************************************** \n\n");
 
 	printf("minimum value is : %d\n",my_heap->min->key);	
 
 	printf("Calling decrease key on node 35 \n");
 	decrease_key(my_heap,35,5);
 
+	printf("\n\n*************************************************** \n\n");
 	print_heap(my_heap->min);
-	
+	printf("\n\n*************************************************** \n\n");
+
+	printf("minimum value is : %d\n",my_heap->min->key);
+
     return 0;
 
 }
--- fibonacci_heap_utils_working.h
+++ fibonacci_heap_utils_working.h
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
 typedef struct node
 {
@@ -15,7 +16,7 @@ typedef struct node
 
 	// extra information
 	int degree;   
-	char mark;   
+	bool mark;   
 	int visited;
 
 }NODE;
@@ -33,26 +34,37 @@ HEAP * make_fib_heap();
 // this function delares a new fibonacci heap
 
 void insert( HEAP * H , int val ); 
-// insert a new node into the fibonacci heap
+// insert a new node into the fibonacci heap with value = val
 
 NODE * find_min( HEAP * H ); 
 // returns the node containing the minimum value in the fibonacci heap 
 
 void print_heap(NODE * H_min);
-// prints the fibonacci heap
+// prints the fibonacci heap -> the input is the pointer to the minimum node of the heap
 
 NODE * find_node(NODE * H_min , int val);
-// return the node with the given val
+// return the node with the given val -> the input is the pointer to the minimum node of the heap and the key of the node to be found
 
 void decrease_key(HEAP * H , int curr_val , int dec_val);
 // decrease the value of a node and reconfigure the heap
 
-void CUT( NODE* H_min , NODE * x , NODE * y );
-void CASCADING_CUT(NODE * H_min , NODE * y);
+void CUT( NODE* H_min , NODE * node , NODE * parent );
+void CASCADING_CUT(NODE * H_min , NODE * parent);
+
+int degree(int n);
+// calculates the maximum degree of a fib_heap with n nodes
+
+void link_heaps(HEAP *H, NODE *y , NODE *x);
+// remove heap with root node as y from the root list and add as a child to the node x
+
+void consolidate (HEAP * H);
+// consolidate the heap 
+
+NODE * ext_min(HEAP * m);
+// extract the minimum element from the heap
 
 
-
-// this function is used for adding nodes at positions other than the root list
+// used for adding nodes at positions other than the root list
 NODE * create_dummy_node(int val)
 {
 	NODE * new_node;
@@ -66,30 +78,37 @@ NODE * create_dummy_node(int val)
 	new_node->key = val;
 
 	new_node->degree = 0;
-	new_node->mark = 'F';
+	new_node->mark = false;
 	new_node->visited = 0; 
 
 	return new_node;
 }
 
+// linking node a and b
+void add_node(NODE * a , NODE * b)
+{
+	( a -> left ) -> right = b;
+	b->right = a;
+	b->left = a->left;
+    a->left = b;
+}
 
 HEAP * make_fib_heap()
 {
 	printf("Making a new fibonacci heap !\n");
 
-	HEAP * H;
+	HEAP * new_heap;
 
-	H = malloc(sizeof(HEAP));
+	new_heap = malloc(sizeof(HEAP));
 
-	H->min = NULL;
+	new_heap->min = NULL;
 
-	H->n = 0;
+	new_heap->n = 0;
 
-	return H;
+	return new_heap;
 }
 
-
-void insert( HEAP * H , int val )  // insert a new node in the fib_heap with value = val
+void insert( HEAP * H , int val ) 
 {
 	NODE * new_node;
 	new_node = malloc(sizeof(NODE));
@@ -102,7 +121,7 @@ void insert( HEAP * H , int val )  // insert a new node in the fib_heap with val
 	new_node->key = val;
 
 	new_node->degree = 0;
-	new_node->mark = 'F';
+	new_node->mark = false;
 	new_node->visited = 0;  
 
 
@@ -116,20 +135,12 @@ void insert( HEAP * H , int val )  // insert a new node in the fib_heap with val
 		return;
 	}
 
-	// adding the new node to the root list as the left sibling of the H->min node
+	// adding the new node to the root list
 	
-	( ( H->min ) -> left ) -> right = new_node;
-
-	new_node->right = H->min;
-
-	new_node->left = (H->min)->left;
-
-    (H->min)->left = new_node;
-
+	add_node(H->min , new_node);
 
     printf("Adding a new node ( val = %d ) to the fibonacci heap in the root list !\n",val);
-    (H->n)++;  // increasing the size by one
-
+    (H->n)++;  // increasing the size of heap by one
 
     if( (new_node->key) < (H->min)->key )  // if the value of the new node is smaller then make it the minimum
     {
@@ -158,17 +169,17 @@ void print_heap(NODE * H_min)
 
 	ptr->visited = 1;
 
-	printf(" %d { degree: %d , mark: %c , visited : %d }\n",ptr->key,ptr->degree,ptr->mark,ptr->visited);
+	printf("  -->  %d 	{ degree : %d , mark : %d }\n",ptr->key,ptr->degree,ptr->mark);
 
 		if( ptr->child != NULL )
 		{
-			printf(" [child of %d] ",ptr->key);
+			printf("[child of %d]",ptr->key);
 			print_heap(ptr->child);
 		}
 
 		if( (ptr->right)->visited != 1 )
 		{
-			printf(" [right of %d] ",ptr->key);
+			printf("[right of %d]",ptr->key);
 			print_heap(ptr->right);
 		}
 
@@ -180,34 +191,28 @@ NODE * find_node(NODE * H_min , int val)
 {
 	NODE * ptr = H_min;
 
-	//printf("The value of node is: %d\n",ptr->key);
-
 	ptr->visited = 1;
-
-	NODE * x = NULL;
 
 	if( ptr->key == val )
 	{
-		//printf("The found node is: %d\n",ptr->key);
-		x = ptr;
 		ptr->visited = 0;
-		return x;
+		return ptr;
 	}
 
-	if( x == NULL )
+	NODE * x = NULL;
+	
+	if( ptr->child != NULL )
 	{
-		if( ptr->child != NULL )
-		{
-			x = find_node(ptr->child,val);
-		}
+		x = find_node(ptr->child,val);
+	}
 
-		if( (ptr->right)->visited != 1 && x == NULL )
-		{
-			x = find_node(ptr->right,val);
-		}
+	if( (ptr->right)->visited != 1 && x == NULL )
+	{
+		x = find_node(ptr->right,val);
 	}
 
 	ptr->visited = 0;
+	
 	return x;
 }
 
@@ -237,14 +242,13 @@ void decrease_key(HEAP * H , int curr_val , int dec_val)
 
 	NODE * parent = found_node->parent;
 
-	found_node->key = dec_val;	
+	found_node->key = dec_val;	// decrease the value of the node
 
 	if( parent != NULL && found_node->key < parent->key )
 	{
 		CUT(H_min , found_node , parent);
 		CASCADING_CUT(H_min , parent);
 	}
-
 
 	if(found_node->key < H->min->key)
 	{
@@ -255,102 +259,127 @@ void decrease_key(HEAP * H , int curr_val , int dec_val)
 
 
 
-void CUT( NODE* H_min , NODE * x , NODE * y )
+void CUT( NODE* H_min , NODE * node , NODE * parent )
 {
-	if( x == x->right )
+	if( node == node->right ) // node is the only child of parent
 	{
-		y->child = NULL;
+		parent->child = NULL;
 	}
 
 	else{
 
-		y->child = x->right;
+		parent->child = node->right;
 
-		(x->left)->right = x->right;
-		(x->right)->left = x->left;
+		(node->left)->right = node->right;
+		(node->right)->left = node->left;
 
 	}
 
-	y->degree = y->degree-1;
+	parent->degree = parent->degree - 1;  // decrease the degree of the parent by 1  
 
-	x->right = x;
-	x->left = x;
+	node->right = node;  
+	node->left = node;
 
-	(H_min->left)->right = x;
-	x->right = H_min;
-	x->left = (H_min->left);
-	H_min->left = x;
+	add_node(H_min , node);	// add node to the root list
 
-	x->parent = NULL;
-	x->mark = 'F';
+	node->parent = NULL;
+	node->mark = false;
 
-	printf("Node of value = %d added to the root list\n",x->key);
+	printf("Node of value = %d added to the root list\n",node->key);
 }
 
-void CASCADING_CUT(NODE * H_min , NODE * y)
+void CASCADING_CUT(NODE * H_min , NODE * parent)
 {
-	NODE* z = y->parent;
+	NODE * parent_parent = parent->parent;
 
-	if(z != NULL)
+	if(parent_parent != NULL)
 	{
-		if(y->mark == 'F')
+		if(parent->mark == false)
 		{
-			y->mark = 'T';
+			parent->mark = true;
 		}
 		else{
 
-			CUT(H_min,y,z);
-			CASCADING_CUT(H_min,z);
+			CUT( H_min , parent , parent_parent );
+			CASCADING_CUT( H_min , parent_parent );
 		}
 	}
 }
 
-
-int degree(int n)
+NODE * ext_min(HEAP * m)
 {
-	int deg=0;
-	while(n>0)
-	{	
-		n=n/2;
-		deg++;
+	int nodesCount = m->n;
+	printf("heap is having %d nodes \n", nodesCount);
+
+	NODE *temp, *rgt;
+
+	if( m->min == NULL)
+		printf("Error ! The Heap Is Empty, cannot extarct the minimum.\n");
+	else
+	{
+		NODE *c =NULL;
+
+		temp=  m->min;
+		rgt = temp;
+		printf("extracted minimum is temp (val = %d)\n",temp->key);
+
+		//adding the child nodes to the root list//
+		if( temp->child !=NULL)
+		{ 
+		    c = temp->child; 
+		    
+		    do 
+		    { 
+		        rgt = c->right; 
+
+		        add_node(m->min , c); 
+
+		        printf ("adding node (val = %d) to root list \n", c->key);
+		                
+		        c->parent = NULL; 
+
+		        //extracting the min value//
+		        if (c->key <  m->min->key) 
+		        	m->min = c; 
+		                 
+		        c = rgt; 
+
+		    } while (rgt != temp->child); 
+		} 
+
+		//extracting the Min Node from the heap//
+    	(temp->right)->left = temp->left; 
+    	(temp->left)->right = temp->right; 
+    	printf ("node (val = %d) removed from the heap \n", temp->key);
+
+    	m->min = temp->right; 
+		if (temp == temp->right && temp->child == NULL) //if min is the only node//
+			m->min = NULL; 
+    	else 
+    	{ 
+    		m->min = temp->right;  //rearrange the tree//
+			printf ("node (val = %d) is the m->min now \n", m->min->key);
+	    	
+	    	consolidate(m); 
+	    } 
+	
+		nodesCount-=1; 
+		m->n = nodesCount;
+    	printf("heap is having %d nodes \n", nodesCount);
 	}
-	return deg;
-}
 
-void link_heaps(HEAP *H, NODE *y , NODE *x)
-{  
-	(y->left)->right = y->right;
-  	(y->right)->left = y->left;
-  
-  	y->left = y;
-  	y->right = y;
-  	y->parent = x;
-
-  	if (x->child == NULL)
-    	x->child = y;
-
-  	y->right = x->child;
-  	y->left = (x->child)->left;
-  	((x->child)->left)->right = y;
-  	(x->child)->left = y;
-
-  	if (y->key < (x->child)->key)
-    	x->child = y;
-  	x->degree++;
-  	y->mark = 'F';
-}
+	return temp; 
+} 
 
 void consolidate (HEAP * H)
 {
-
 	int i;
 	int dMax,d;
 
 	dMax = degree(H->n);
 
 	NODE * a[dMax], *x,*y,*temp;
-	printf("max degree of a node in H is = %d \n", (dMax-1));
-
+	printf("\nmax degree of a node in H is = %d \n", (dMax-1));
  
 	for ( i=0 ; i<dMax ; i++)
 	{
@@ -366,7 +395,7 @@ void consolidate (HEAP * H)
 		d = x->degree ;
 		printf ("degree of node (val = %d) is (d = %d) \n", x->key, d);
 		
-		while(a[d]!=NULL && a[d]->key != x->key )
+		while(a[d]!=NULL && x->key != a[d]->key )
 		{
 			y = a[d];     
 			printf ("found node with same degree(%d) having (val = %d) \n",d, y->key);
@@ -384,12 +413,14 @@ void consolidate (HEAP * H)
 			d = d+1;
 		}
 	
-		a[d] = x;    //a[d] is node having degree d
+		a[d] = x;    //x is node having degree d
+
 		x = x->right;
 
 	}while(x != H->min);
 
 	printf ("visited all nodes in the root list \n");
+
 
 
 	H->min = (NODE *)NULL;
@@ -403,100 +434,48 @@ void consolidate (HEAP * H)
 
 			if(H->min != (NODE *)NULL)
 			{
-				( H->min -> left ) -> right = a[i];
-				a[i]->right = H->min;
-				a[i]->left = (H->min)->left;
-				(H->min)->left = a[i];
+				add_node(H->min , a[i]);
 
-				printf ("value of H->min node is %d \n", H->min->key);
-				
-			if( a[i]->key < H->min->key )
+				if( a[i]->key < H->min->key )
 				H->min = a[i]; //updating H->min
+			}
+			else
+			{
+				H->min = a[i];
+			}
 		}
-		else
-		{
-			H->min = a[i];
-			printf ("adding node (val = %d) to root list \n", a[i]->key);
-		}
-
-		if( H->min == NULL )
-		{
-			H->min = a[i];
-		}
-		else if( a[i]->key < H->min->key )
-		{
-			H->min = a[i];
-		}
-
 	}
-}
-
-
+	
 	return;
-
 }
 
-
-
-NODE * ext_min(HEAP * m) {
-
-	int nodesCount = m->n;
-	printf("heap is having %d nodes \n", nodesCount);
-
-	NODE *temp, *rgt;
-
-	if( m->min == NULL)
-		printf("Error ! Your Heap Is Empty, cannot extarct the minimum.\n");
-	else
-	{
-		NODE *c =NULL;
-		//NODE *temp, *rgt;
-		temp=  m->min;
-		rgt = temp;
-		printf("extracted minimum is temp (val = %d)\n",temp->key);
-		//adding the child nodes to the root list//
-		if( temp->child !=NULL)
-		 { 
-		  
-		            c = temp->child; 
-		            do { 
-		                rgt = c->right; 
-
-		                ( m->min->left)->right = c; 
-		                c->left =  m->min->left;
-		                m->min->left = c;
-		                c->right = m->min; 
-		                printf ("adding node (val = %d) to root list \n", c->key);
-		                 
-		                //extracting the min value//
-		                if (c->key <  m->min->key) 
-		                	 m->min = c; 
-		                c->parent = NULL; 
-		                c = rgt; 
-		            } while (rgt != temp->child); 
-		    } 
-
-		//extracting the Min Node//
-    (temp->right)->left = temp->left; 
-    (temp->left)->right = temp->right; 
-    printf ("node (val = %d) removed from the heap \n", temp->key);
-
-    m->min = temp->right; 
-	if (temp == temp->right && temp->child == NULL) //if min is the only node//
-		 m->min = NULL; 
-    else { 
-    	  m->min = temp->right;  //rearrange the tree//
-		  printf ("node (val = %d) is the m->min now \n", m->min->key);
-	      consolidate(m); 
-	     } 
-	nodesCount-=1; 
-	m->n = nodesCount;
-    printf("heap is having %d nodes \n", nodesCount);
+int degree(int n)
+{
+	int deg=0;
+	while(n>0)
+	{	
+		n=n/2;
+		deg++;
 	}
+	return deg;
+}
 
-	return temp; 
+void link_heaps(HEAP *H, NODE *y , NODE *x)
+{  
+	(y->left)->right = y->right;  // remove y from root list of H
+  	(y->right)->left = y->left;
+  	y->left = y;
+  	y->right = y;
 
-} 
+  	y->parent = x;   // make y a child of x
 
+  	if (x->child == NULL)  // if x has no child previously
+    	x->child = y;
 
+    // if x already has a child list, add y to the child list
 
+    add_node(x->child , y);
+
+  	x->degree++;
+  	y->mark = false;
+}
